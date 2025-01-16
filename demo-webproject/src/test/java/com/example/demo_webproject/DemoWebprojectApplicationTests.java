69f8187cc85ef28daa33ab4dbf989df81e19f728
HarishNS <harishnhs288007@gmail.com>
2025-01-16 10:57:17 +0530

    commiting project foilder

---
+++ demo-webproject/src/test/java/com/example/demo_webproject/DemoWebprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
